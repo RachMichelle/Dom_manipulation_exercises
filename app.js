a9eb765fe79a59573ce7128639358401c154fc34
Rachel <rachel@LAPTOP-2HAP8AE4.localdomain>
2024-03-25 14:32:30 -0400

    added js

--- app.js
+++ app.js
@@ -10,3 +10,46 @@
 // 10. Append the li to the ul element.
 // 11. Loop over all of the lis inside the ol tag and give them a background color of “green”.
 // 12. Remove the div with a class of footer
+
+//1
+document.getElementById("container");
+
+//2
+const container = document.querySelector("#container");
+
+//3
+const second = document.getElementsByClassName("second");
+
+//4
+const olThird = document.querySelector("ol .third");
+
+//5 (commented out to be able to do later questions--replaces ol/ul)
+// container.innerText = "Hello!";
+
+//6
+const footer = document.querySelector(".footer");
+footer.classList.add('main');
+
+//7
+footer.classList.remove('main');
+
+//8
+const newLi = document.createElement('li');
+
+//9
+newLi.innerText = "four";
+
+//10
+const ul = document.querySelector('ul');
+ul.append(newLi);
+
+//11
+const ol = document.querySelector('ol');
+const orderedLis = document.querySelectorAll('ol li');
+
+for (let li of orderedLis){
+    li.style.backgroundColor = "green";
+}
+
+//12
+footer.remove();
